d2a6d5a7eb9435f79521dacbf8b44afcf9441853
Cory Burditt <26898430+coryburdit@users.noreply.github.com>
2021-12-30 15:53:18 -0600

    added Run as admin and completion

--- adsync.ps1
+++ adsync.ps1
@@ -4,5 +4,21 @@
 # Date: March 16  , 2018                                                       #
 # Description: This script Forces a sync with Azure AD of any changes made     #
 ################################################################################
+
+
+#Are you admin No then RuN AS ADMIN 
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+#The Commands
 Import-Module AdSync
 Start-ADSyncSyncCycle -PolicyType Delta
+
+#Completion notice and exit prompt.
+Write-Host ("Completed" )
+write-host "Press any key to close..."
+[void][System.Console]::ReadKey($true)
